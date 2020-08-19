65f5c413619b2318a767deaaf7c61fbf6b2b7f19
David Dollar <david@dollar.io>
2020-08-19 17:38:26 -0400

    wip

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -10,13 +10,6 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "prettier/prettier": [
-      "error",
-      {
-        trailingComma: "es5",
-        printWidth: 140,
-      },
-    ],
   },
   overrides: [
     {
