03e178a1b5497ff6a9606b7c9797f17b3a47e9a4
David Dollar <david@dollar.io>
2020-09-29 09:41:56 -0400

    wip

--- web/src/views/Public/Login.vue
+++ web/src/views/Public/Login.vue
@@ -2,34 +2,36 @@
   <div class="row">
     <div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
       <div class="card">
-        <div class="card-header">Login</div>
-        <div class="position-relative d-flex">
-          <div class="card-body">
-            <div v-if="alert" class="alert alert-danger" role="alert">{{ alert }}</div>
-            <div class="form-group">
-              <label for="login-email">Email</label>
-              <input id="login-email" class="form-control" type="email" v-model="email" />
+        <form @submit="submit">
+          <div class="card-header">Login</div>
+          <div class="position-relative d-flex">
+            <div class="card-body">
+              <div v-if="alert" class="alert alert-danger" role="alert">{{ alert }}</div>
+              <div class="form-group">
+                <label for="login-email">Email</label>
+                <input id="login-email" class="form-control" type="email" v-model="email" />
+              </div>
+              <div class="form-group">
+                <label for="login-password">Password</label>
+                <input id="login-password" class="form-control" type="password" v-model="password" />
+              </div>
             </div>
-            <div class="form-group">
-              <label for="login-password">Password</label>
-              <input id="login-password" class="form-control" type="password" v-model="password" />
+            <div
+              id="login-alert"
+              class="alert alert-danger rounded-0 h-100 w-100 position-absolute d-none align-items-center justify-content-center"
+            ></div>
+          </div>
+          <div class="card-footer d-flex align-items-center">
+            <div class="flex-fill">
+              <a href>Forgot Password</a>
+            </div>
+            <div class="flex-fill text-right">
+              <b-button type="submit" id="login" variant="primary">
+                Login
+              </b-button>
             </div>
           </div>
-          <div
-            id="login-alert"
-            class="alert alert-danger rounded-0 h-100 w-100 position-absolute d-none align-items-center justify-content-center"
-          ></div>
-        </div>
-        <div class="card-footer d-flex align-items-center">
-          <div class="flex-fill">
-            <a href>Forgot Password</a>
-          </div>
-          <div class="flex-fill text-right">
-            <b-button id="login" variant="primary" @click="submit()">
-              Login
-            </b-button>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
     <b-modal id="token-authenticate" hide-header hide-footer>
@@ -58,7 +60,8 @@ export default {
     };
   },
   methods: {
-    submit() {
+    submit(e) {
+      e.preventDefault();
       this.alert = "";
       this.$apollo
         .mutate({
