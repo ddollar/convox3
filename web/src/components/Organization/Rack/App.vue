85d91d35f04cca50ec8476a4d2dfef09cb4f1e32
David Dollar <david@dollar.io>
2020-09-29 10:17:23 -0400

    wip

--- web/src/components/Organization/Rack/App.vue
+++ web/src/components/Organization/Rack/App.vue
@@ -11,58 +11,6 @@
       <b-button variant="danger" size="sm"><i class="fa fa-times" style="font-size:1.1em; padding-left:2px; padding-right:2px;"/></b-button>
     </td>
   </tr>
-
-  <!-- <div class="col-12 col-xl-6 col-xxl-4 app clickable" @click="goto()">
-    <div class="card mb-4 border-bottom-0">
-      <div class="card-header d-flex bg-light">
-        <div class="flex-grow-1">
-          <i class="fa far fa-window-maximize mr-1" />
-          {{ app.name }}
-        </div>
-        <div class="flex-shrink-0">
-          <Status :status="app.status" color />
-        </div>
-      </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item d-flex align-items-center p-0">
-          <div class="flex-even p-3 border-right">
-            <div class="font-weight-bold">Processes</div>
-            <i v-if="$apollo.queries.processes.loading" class="spinner"></i>
-            <div v-else>{{ processes.length }}</div>
-          </div>
-          <div class="flex-even p-3 border-right">
-            <div class="font-weight-bold">CPU</div>
-            <i v-if="$apollo.queries.services.loading" class="spinner"></i>
-            <div v-else>{{ cpu }}</div>
-          </div>
-          <div class="flex-even p-3">
-            <div class="font-weight-bold">Memory</div>
-            <i
-              v-if="$apollo.queries.services.loading"
-              class="fas fa-circle-notch fa-spin text-muted"
-            ></i>
-            <div v-else>{{ mem }}</div>
-          </div>
-        </li>
-        <li class="list-group-item p-0">
-          <div class="row" style="padding-left: 15px; padding-right: 14px;">
-            <div class="col-12 col-xxl-6 p-3 border-right border-bottom bg-light">
-              <div
-                style="width: 100%; background-color: #fff; height: 80px; border: 1px #eee solid;"
-                class="d-flex align-items-center justify-content-center text-secondary"
-              >CPU/Memory Graph</div>
-            </div>
-            <div class="col-12 col-xxl-6 p-3 border-right border-bottom bg-light">
-              <div
-                style="width: 100%; background-color: #fff; height: 80px; border: 1px #eee solid;"
-                class="d-flex align-items-center justify-content-center text-secondary"
-              >Network Graph</div>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </div> -->
 </template>
 
 <script>
@@ -95,11 +43,13 @@ export default {
   },
   components: {
     Async: () => import("@/components/Async.vue"),
-    // Status: () => import("@/components/Organization/Rack/App/Status.vue"),
   },
   computed: {
     cpu() {
       return this.services.reduce((ax, s) => ax + s.cpu * s.count, 0);
+    },
+    id() {
+      return `app-${this._uid}`;
     },
     mem() {
       return this.pretty_memory(this.services.reduce((ax, s) => ax + s.mem * s.count, 0));
